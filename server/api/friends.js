0e162e5c397b781da2d38084347de911df4f9cf8
Anisah Leila <anisahleila@hotmail.com>
2022-12-10 16:22:31 +0000

    attending table works

--- server/api/friends.js
+++ server/api/friends.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express.Router();
-const { User, Friendship } = require("../db");
+const { User, Friendship, Attending } = require("../db");
 const { isLoggedIn } = require("./middleware");
 
 module.exports = app;
@@ -35,6 +35,10 @@ app.get("/", async (req, res, next) => {
               attributes: {
                 exclude: ["password", "address", "addressDetails"],
               },
+            },
+            {
+              model: Attending,
+              // as: "attending",
             },
           ],
         })
--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,10 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 import auth from "./auth";
-import events from "./events";
 import users from "./users";
 
 const reducer = combineReducers({
   auth,
-  events,
   users,
 });
 
@@ -16,5 +14,4 @@ const store = createStore(reducer, applyMiddleware(thunk, logger));
 export default store;
 
 export * from "./auth";
-export * from "./events";
 export * from "./users";
