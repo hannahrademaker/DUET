3f630d5c83f180646947ec0279065fc1b66a48c7
alexaronis <aronis.alex@gmail.com>
2022-12-17 17:23:37 +0000

    always be defensive

--- src/Components/FriendPage.js
+++ src/Components/FriendPage.js
@@ -5,18 +5,17 @@ import {
   sendFriendRequest,
   deleteFriendship,
   fetchFriendships,
+  fetchUsers,
 } from "../store";
 import UserEvents from "./UserEvents";
 
 const FriendPage = () => {
-  const { users, auth, friendships } = useSelector((state) => state);
   const { id } = useParams();
+  const { users, auth, friendships } = useSelector((state) => state);
   const dispatch = useDispatch();
   const [toggle, setToggle] = useState(false);
 
-  useEffect(() => {
-    dispatch(fetchFriendships());
-  }, []);
+  const friend = users.find((user) => id === user.id);
 
   const destroyFriendship = (friend) => {
     let friendship = friendships.find(
@@ -35,185 +34,168 @@ const FriendPage = () => {
   //   (invite) => invite.friendship.status === "pending"
   // );
   // const outboxIds = outbox.map((outboxId) => outboxId.id);
+  const confirmedFriends = friendships.filter((friendship) => {
+    if (
+      friendship.status === "accepted" &&
+      friendship.ids.includes(friend.id)
+    ) {
+      return friendship;
+    }
+  });
+  const friendsOfFriends = users.reduce((acc, user) => {
+    for (let i = 0; i < confirmedFriends.length; i++) {
+      if (confirmedFriends[i].ids.includes(user.id) && user.id !== friend.id) {
+        acc.push(user);
+      }
+    }
+    return acc;
+  }, []);
 
-  return (
-    <div id="user-page">
-      {users.map((friend) => {
-        if (friend.id === id) {
-          const confirmedFriends = friendships.filter((friendship) => {
-            if (
-              friendship.status === "accepted" &&
-              friendship.ids.includes(friend.id)
-            ) {
-              return friendship;
-            }
-          });
+  // let friendList = friend.Accepter.concat(friend.Requester).filter(
+  //   (friend) => friend.friendship.status === "accepted"
+  // );
+  // console.log(friendList);
+  // let pendingFriendList = auth.Accepter.concat(auth.Requester).filter(
+  //   (friend) => friend.friendship.status === "pending"
+  // );
+  //console.log(pendingFriendList);
+  let requested = false;
+  const friendsIds = friendsOfFriends.map((myFriendsId) => myFriendsId.id);
 
-          const friendsOfFriends = users.reduce((acc, user) => {
-            for (let i = 0; i < confirmedFriends.length; i++) {
-              if (
-                confirmedFriends[i].ids.includes(user.id) &&
-                user.id !== friend.id
-              ) {
-                acc.push(user);
-              }
-            }
-            return acc;
-          }, []);
+  // const friendListIds = friendList.map((friendId) => friendId.id);
+  if (!friend) return null;
+  if (friend)
+    return (
+      <div>
+        <div className="username-top">
+          <h3>{friend.username}</h3>
+        </div>
+        <div className="profile-page-details-top">
+          {friend.img && (
+            <img
+              src={friend.img}
+              alt="Pic of friend"
+              width="200"
+              height="200"
+            />
+          )}
+          {!friend.img && (
+            <img
+              src="../static/DUET/blankprofile.png"
+              alt="blank profile"
+              width="200"
+              height="200"
+            />
+          )}
+        </div>
+        <div>
+          <span>Events ()</span>
+          <span>Friends ({friendsOfFriends.length})</span>
+        </div>
+        <div>
+          <h4>
+            {friend.firstName} {friend.lastName}
+          </h4>
+          <p>{friend.bio}</p>
+        </div>
 
-          // let friendList = friend.Accepter.concat(friend.Requester).filter(
-          //   (friend) => friend.friendship.status === "accepted"
-          // );
-          //console.log(friendList);
-          // let pendingFriendList = auth.Accepter.concat(auth.Requester).filter(
-          //   (friend) => friend.friendship.status === "pending"
-          // );
-          // //console.log(pendingFriendList);
-          // let requested = false;
-          const friendsIds = friendsOfFriends.map(
-            (myFriendsId) => myFriendsId.id
-          );
-
-          //const friendListIds = friendList.map((friendId) => friendId.id);
-          // <button onClick={() => dispatch(blockUser(friend))}>
-          //                 Block {friend.username}
-          //               </button>
-          return (
-            <div key={friend.id}>
-              <div className="username-top">
-                <h3>{friend.username}</h3>
-              </div>
-              <div className="profile-page-details-top">
-                {friend.img && (
-                  <img
-                    src={friend.img}
-                    alt="Pic of friend"
-                    width="200"
-                    height="200"
-                  />
-                )}
-                {!friend.img && (
-                  <img
-                    src="../static/DUET/blankprofile.png"
-                    alt="blank profile"
-                    width="200"
-                    height="200"
-                  />
-                )}
-              </div>
-              <div>
-                <span>Events ()</span>
-                <span>Friends ({friendsOfFriends.length})</span>
-              </div>
-              <div>
-                <h4>
-                  {friend.firstName} {friend.lastName}
-                </h4>
-                <p>{friend.bio}</p>
-              </div>
-
-              <div className="list-6-friends">
-                <div>
-                  <h5>Friends</h5>
-                  {friendsOfFriends.map((friendOfFriend) => {
-                    if (friendOfFriend.id !== auth.id) {
-                      return (
-                        <div key={friendOfFriend.id} className="friend-card">
-                          <ul>
-                            <li>
-                              <Link to={`/users/${friendOfFriend.id}`}>
-                                {friendOfFriend.username}
-                              </Link>
-                              {friendOfFriend.img && (
-                                <img
-                                  className="people-you-may-know-img"
-                                  src={friendOfFriend.img}
-                                  alt="Pic of Friend"
-                                  width="200"
-                                  height="200"
-                                />
-                              )}
-                              {!friendOfFriend.img && (
-                                <img
-                                  className="people-you-may-know-img"
-                                  src="../static/DUET/blankprofile.png"
-                                  alt="blank profile"
-                                  width="200"
-                                  height="200"
-                                />
-                              )}
-                            </li>
-                          </ul>
-                        </div>
-                      );
-                    } else if (friendOfFriend.id === auth.id) {
-                      return (
-                        <div key={auth.id} className="friend-card">
-                          <ul>
-                            <li>
-                              <Link to={`/user`}>{auth.username}</Link>
-                              <img
-                                className="people-you-may-know-img"
-                                src={auth.img}
-                                alt="Pic of Friend"
-                                width="200"
-                                height="200"
-                              />
-                            </li>
-                          </ul>
-                        </div>
-                      );
-                    }
-                  })}
-                </div>
-              </div>
-              <div className="toggle-user-details">
-                {!toggle && (
-                  <button
-                    className="see-user-details-button"
-                    onClick={() => {
-                      setToggle(!toggle);
-                    }}
-                  >
-                    See Friend Info
-                  </button>
-                )}
-                {toggle && (
-                  <div className="user-details">
-                    <div>
-                      <h4>Email address</h4>
-                      <p>{friend.email}</p>
-                    </div>
-                    <h4>Address</h4>
-                    <p>
-                      {friend.address} {friend.addressDetails}
-                    </p>
-                    <p>
-                      {friend.city}, {friend.state} {friend.zip}
-                    </p>
-
-                    <button onClick={() => destroyFriendship(friend)}>
-                      Unfriend
-                    </button>
-                    <br />
-                    <button
-                      className="hide-user-details-button"
-                      onClick={() => {
-                        setToggle(!toggle);
-                      }}
-                    >
-                      Hide {friend.username}'s Info'
-                    </button>
+        <div className="list-6-friends">
+          <div>
+            <h5>Friends</h5>
+            {friendsOfFriends.map((friendOfFriend) => {
+              if (friendOfFriend.id !== auth.id) {
+                return (
+                  <div key={friendOfFriend.id} className="friend-card">
+                    <ul>
+                      <li>
+                        <Link to={`/users/${friendOfFriend.id}`}>
+                          {friendOfFriend.username}
+                        </Link>
+                        {friendOfFriend.img && (
+                          <img
+                            className="people-you-may-know-img"
+                            src={friendOfFriend.img}
+                            alt="Pic of Friend"
+                            width="200"
+                            height="200"
+                          />
+                        )}
+                        {!friendOfFriend.img && (
+                          <img
+                            className="people-you-may-know-img"
+                            src="../static/DUET/blankprofile.png"
+                            alt="blank profile"
+                            width="200"
+                            height="200"
+                          />
+                        )}
+                      </li>
+                    </ul>
                   </div>
-                )}
+                );
+              } else if (friendOfFriend.id === auth.id) {
+                return (
+                  <div key={auth.id} className="friend-card">
+                    <ul>
+                      <li>
+                        <Link to={`/user`}>{auth.username}</Link>
+                        <img
+                          className="people-you-may-know-img"
+                          src={auth.img}
+                          alt="Pic of Friend"
+                          width="200"
+                          height="200"
+                        />
+                      </li>
+                    </ul>
+                  </div>
+                );
+              }
+            })}
+          </div>
+        </div>
+        <div className="toggle-user-details">
+          {!toggle && (
+            <button
+              className="see-user-details-button"
+              onClick={() => {
+                setToggle(!toggle);
+              }}
+            >
+              See Friend Info
+            </button>
+          )}
+          {toggle && (
+            <div className="user-details">
+              <div>
+                <h4>Email address</h4>
+                <p>{friend.email}</p>
               </div>
+              <h4>Address</h4>
+              <p>
+                {friend.address} {friend.addressDetails}
+              </p>
+              <p>
+                {friend.city}, {friend.state} {friend.zip}
+              </p>
+
+              <button onClick={() => destroyFriendship(friend)}>
+                Unfriend
+              </button>
+              <br />
+              <button
+                className="hide-user-details-button"
+                onClick={() => {
+                  setToggle(!toggle);
+                }}
+              >
+                Hide {friend.username}'s Info'
+              </button>
             </div>
-          );
-        }
-      })}
-      {/* <PplMayKnow /> */}
-    </div>
-  );
+          )}
+        </div>
+      </div>
+    );
 };
 
 export default FriendPage;
